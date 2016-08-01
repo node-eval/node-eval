7ece853ae79de730bee29677bde77a19460693b1
Vasiliy Loginevskiy <yeti-or@yandex-team.ru>
2016-08-02 01:24:26 +0300

    Refactor tests

--- test/test.js
+++ test/test.js
@@ -4,75 +4,91 @@ var expect = chai.expect;
 var nodeEval = require('../');
 
 function coco(content, context) {
-    var path;
-    if(typeof content === 'string') {
-        path = 'file.js';
-    } else {
-        path = 'file.json';
-        content = JSON.stringify(content);
-    }
-    return expect(nodeEval(content, path, context)).to;
+    return expect(nodeEval(content, 'file.js', context)).to;
 }
 
-it('should eval simple expression', () => coco('42 / 42').eql(1));
+describe('expression', () => {
+    it('should eval simple expression', () => coco('42 / 42').eql(1));
 
-it('should eval expression', () => coco('({42:42})').eql({42: 42}));
+    it('should eval expression', () => coco('({42:42})').eql({42: 42}));
 
-it('should not eval simple object', () => coco('{}').not.to.exist);
-
-it('should eval module.exports', () => coco('module.exports = {42:42}').eql({42: 42}));
-
-it('should eval exports', () => coco('exports.block = 42').eql({block: 42}));
-
-it('should eval exports replace', () => coco('exports = {42:42}').eql({42: 42}));
-
-it('should eval exports = module.exports', () => coco('exports = module.exports = {42:42}').eql({42: 42}));
-
-it('should eval module with require', () => {
-    var requireContent =
-    `
-        var p = require('../package.json');
-        module.exports = {
-            block: p.name,
-        };
-    `;
-    coco(requireContent).eql({block: 'node-eval'});
+    it('should not eval simple object', () => coco('{}').to.undefined);
 });
 
-var answer = 42;
+describe('commonJS modules', () => {
+    it('should eval exports', () => coco('exports.block = 42; ({1:1, 2:2});').eql({block: 42}));
 
-it('should provide global objects', () => coco('answer + answer', {answer}).to.eql(84));
+    it('should eval exports number', () => coco('exports = 42; ({1:1, 2:2});').eql(42));
 
-it('should provide global objects to module syntax', () => coco('module.exports = typeof it + typeof chai', {it, chai}).to.eql('functionobject'));
+    it('should eval exports bool', () => coco('exports = true; ({1:1, 2:2});').eql(true));
 
-it('should eval json', () => coco({42: 42}).eql({42: 42}));
+    it('should eval exports string', () => coco('exports = \'42\'; ({1:1, 2:2});').eql('42'));
 
-it('should work without filename argument', () => expect(nodeEval('42')).to.eql(42));
+    it('should eval exports undefined', () => coco('exports = undefined; ({1:1, 2:2});').eql(undefined));
 
-it('should throw on bad json', function() {
-    var path = 'file.json',
-        content = '{"block":"page",}';
+    it('should eval exports {}', () => coco('exports = {}; ({1:1, 2:2});').eql({}));
 
-    expect(() => nodeEval(content, path)).to.throw(Error);
+    it('should eval exports replace', () => coco('exports = {42:42, 24:24, 1:1}; ({1:1, 2:2});').eql({42: 42, 24: 24, 1: 1}));
+
+    it('should eval exports = module.exports', () => coco('exports = module.exports = {42:42}; ({1:1, 2:2});').eql({42: 42}));
+
+    it('should eval module.exports', () => coco('module.exports = {42:42}; ({1:1, 2:2});').eql({42: 42}));
+
+    it('should return empty object if you touch `module` object but didn\'t export anything', () => {
+        expect(nodeEval('module.zxqfox = {42:42}; ({1:1, 2:2});')).to.eql({});
+    });
+
+    it('should eval module with require', () => {
+        var requireContent =
+        `
+            var p = require('../package.json');
+            module.exports = {
+                block: p.name,
+            };
+        `;
+        coco(requireContent).eql({block: 'node-eval'});
+    });
 });
 
-it('should throw on bad js', function() {
-    var path = 'file.js',
-        content = 'throw new Error("Hello")';
+describe('JSON', () => {
+    it('should eval json', () => {
+        var path = 'file.json',
+            content = '{"42":42}';
 
-    expect(() => nodeEval(content, path)).to.throw('Hello');
+        expect(nodeEval(content, path)).to.eql({42: 42});
+    });
+
+    it('should throw on bad json', () => {
+        var path = 'file.json',
+            content = '{"block":"page",}';
+
+        expect(() => nodeEval(content, path)).to.throw(Error);
+    });
 });
 
-it('should not throw on touching `module` in expression statements', function() {
-    expect(() => nodeEval('module.zxqfox = {42:42}')).not.to.throw(Error);
+describe('context', () => {
+    var answer = 42;
+
+    it('should provide context objects', () => coco('answer + answer', {answer}).to.eql(84));
+
+    it('should provide context objects to module syntax', () => coco('module.exports = typeof it + typeof chai', {it, chai}).to.eql('functionobject'));
 });
 
-it('should not polute global.module obj', function() {
-    global.module = {42: 42};
-    nodeEval('module.zxqfox = {42:42}');
-    expect(global.module).to.eql({42: 42});
+describe('common', () => {
+    it('should work without filename argument', () => expect(nodeEval('42')).to.eql(42));
+
+    it('should not polute global.module obj', function() {
+        global.module = {42: 42};
+        nodeEval('module.zxqfox = {42:42}');
+        expect(global.module).to.eql({42: 42});
+    });
 });
 
-it('should return empty object if you touch `module` object but didn\'t export anything', function() {
-    expect(nodeEval('module.zxqfox = {42:42}')).to.eql({});
+describe('errors', () => {
+    xit('should throw on bad js', function() {
+        var path = 'file.js',
+            content = 'throw new Error("Hello")';
+
+        expect(() => nodeEval(content, path)).to.throw('Hello');
+    });
 });
