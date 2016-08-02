edaa0b64226f3fd206669c3350b864fb95ab709b
Vasiliy Loginevskiy <yeti-or@yandex-team.ru>
2016-08-02 14:23:56 +0300

    Fix module.exports = 'primitive'

--- test/test.js
+++ test/test.js
@@ -18,21 +18,23 @@ describe('expression', () => {
 describe('commonJS modules', () => {
     it('should eval exports', () => coco('exports.block = 42; ({1:1, 2:2});').eql({block: 42}));
 
-    it('should eval exports number', () => coco('exports = 42; ({1:1, 2:2});').eql(42));
+    it('should eval module.exports', () => coco('module.exports = {42:42}; ({1:1, 2:2});').eql({42: 42}));
 
-    it('should eval exports bool', () => coco('exports = true; ({1:1, 2:2});').eql(true));
+    it('should eval module.exports number', () => coco('module.exports = 42; ({1:1, 2:2});').eql(42));
 
-    it('should eval exports string', () => coco('exports = \'42\'; ({1:1, 2:2});').eql('42'));
+    it('should eval module.exports bool', () => coco('module.exports = true; ({1:1, 2:2});').eql(true));
 
-    it('should eval exports undefined', () => coco('exports = undefined; ({1:1, 2:2});').eql(undefined));
+    it('should eval module.exports bool', () => coco('module.exports = false; ({1:1, 2:2});').eql(false));
 
-    it('should eval exports {}', () => coco('exports = {}; ({1:1, 2:2});').eql({}));
+    it('should eval module.exports string', () => coco('module.exports = \'42\'; ({1:1, 2:2});').eql('42'));
 
-    it('should eval exports replace', () => coco('exports = {42:42, 24:24, 1:1}; ({1:1, 2:2});').eql({42: 42, 24: 24, 1: 1}));
+    it('should eval module.exports undefined', () => coco('module.exports = undefined; ({1:1, 2:2});').eql(undefined));
+
+    it('should eval module.exports replace', () => coco('module.exports = {42:42, 24:24, 1:1}; ({1:1, 2:2});').eql({42: 42, 24: 24, 1: 1}));
+
+    it('should eval module.exports {}', () => coco('module.exports = {}; ({1:1, 2:2});').eql({}));
 
     it('should eval exports = module.exports', () => coco('exports = module.exports = {42:42}; ({1:1, 2:2});').eql({42: 42}));
-
-    it('should eval module.exports', () => coco('module.exports = {42:42}; ({1:1, 2:2});').eql({42: 42}));
 
     it('should return empty object if you touch `module` object but didn\'t export anything', () => {
         expect(nodeEval('module.zxqfox = {42:42}; ({1:1, 2:2});')).to.eql({});
